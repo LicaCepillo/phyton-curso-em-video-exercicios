43b48d945af853c1ba4d22596db5750ca66db238
Lilian Lima <liliancepillo@gmail.com>
2022-07-15 11:30:56 +0100

    Alteração da cotação do dolar

--- Pythonexercicios/ex010.py
+++ Pythonexercicios/ex010.py
@@ -1,4 +1,4 @@
 # 'exercicio de conversor de moedas - Real em Dolar '#
 moneyBR = float(input('Quanto dinheiro você tem na carteira ? R$ '))
-dollar = moneyBR / 5.67
+dollar = moneyBR / 3.67
 print('Com R$ {}, você pode comprar USD$ {:.2f}'.format(moneyBR, dollar))
