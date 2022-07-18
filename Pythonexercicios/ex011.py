4373adc041d777f75889daa97fe59c255a481648
Lilian Lima <liliancepillo@gmail.com>
2022-07-18 23:19:31 +0100

    mais exrercicios python

---
+++ Pythonexercicios/ex011.py
@@ -0,0 +1,7 @@
+largura = float(input('Largura da Parede:'))
+altura = float(input('Altura da Parede:'))
+area = (largura * altura)
+lata = (1 * area)/2
+print('Sua parede tem {} x {} e sua área é {} m2'.format(largura, altura, area))
+print('Para pintar esta parede vocè usará {} Litros de tinta'.format(lata))
+
