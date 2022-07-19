f68ecf5871621f6700908992811babaca5fa5a15
Lilian Lima <liliancepillo@gmail.com>
2022-07-19 16:41:37 +0100

    Create ex014.py

---
+++ Pythonexercicios/ex014.py
@@ -0,0 +1,3 @@
+tempcelcius = float(input('Digite a temperatur em graus Celcius: '))
+tempfareheit = ((tempcelcius * 9)/5) + 32
+print(' {} ºC, corresponde a {} º F'.format(tempcelcius, tempfareheit))
