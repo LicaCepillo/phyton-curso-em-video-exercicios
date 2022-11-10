aabbd01f6dfd55e10538eb15438db2fd3f72f6c2
Lilian Lima <liliancepillo@gmail.com>
2022-11-10 16:22:51 +0000

    exercícios
    exercicios

---
+++ Pythonexercicios/ex0016b.py
@@ -0,0 +1,9 @@
+## resolução com importação de biblioteca 
+"""from math import trunc
+num = float(input('Digite aqui um número real :'))
+inteiro = trunc(num)
+print('O número digitado foi {} e sua porção inteira é {}'.format (num, trunc(inteiro)))"""
+
+## resolução sem importação de biblioteca 
+num = float(input("Digite aqui um número real :"))
+print("O valor digitado do {} e sua porção inteira é {}".format(num, int(num)))
